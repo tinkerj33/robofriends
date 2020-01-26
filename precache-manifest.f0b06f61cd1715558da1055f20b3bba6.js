508c79344169f3df41fdff4aedd781f7862b6ac1
tinkerj33 <tinkerj33@gmail.com>
2020-01-25 21:21:43 -0800

    Updates

---
+++ precache-manifest.f0b06f61cd1715558da1055f20b3bba6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b4c3346a86d88f592dfca0f1a7fb033",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "88bae5fcb27fcad9d35b",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8c35e2c6a85c92ae40ad",
+    "url": "/robofriends/static/css/main.1445f38b.chunk.css"
+  },
+  {
+    "revision": "88bae5fcb27fcad9d35b",
+    "url": "/robofriends/static/js/2.2475d6f8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.2475d6f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c35e2c6a85c92ae40ad",
+    "url": "/robofriends/static/js/main.b593e3c9.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
